5237304a4758b2a22002b6934d64beadfd0b9221
Baris Tekir <trexepta@gmail.com>
2025-02-22 11:52:44 +0300

    Initial commit.

---
+++ src/main/java/pixel/academy/spring_container/SpringContainerApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.spring_container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContainerApplication.class, args);
+	}
+
+}
